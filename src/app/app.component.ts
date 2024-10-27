cb93080983609129b7dc4bb8fcf746a8427f9fc0
adityabanik2004 <adityabanik2004@gmail.com>
2024-10-27 13:05:53 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first';
+  uname=new FormControl('')
+
+  updateName()
+  {
+    this.uname.setValue('Amitava')
+  }
+
 }
